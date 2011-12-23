a2f7aec4f3b69cd4bd1082c3d8683abe79091cc3
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-12-23 10:09:09 -0500

    Added a file to duplicate trees.

---
+++ duplicate-project-tree.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+path=$1
+
+projectName=$(basename $path)
+
+mkdir $projectName
+
+for sample in $(ls $path)
+do
+	mkdir $projectName/$sample
+	cd $projectName/$sample
+	for i in $(find ../../$path|grep fastq)
+	do
+		ln -s $i
+	done
+	cd ../../
+done
+
