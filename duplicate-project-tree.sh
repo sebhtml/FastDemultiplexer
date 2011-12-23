dae042af681b64db0768e481fbc0af48f0251b86
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2011-12-23 14:16:12 -0500

    Fixed a bug in the tree copier.

--- duplicate-project-tree.sh
+++ duplicate-project-tree.sh
@@ -10,7 +10,7 @@ for sample in $(ls $path)
 do
 	mkdir $projectName/$sample
 	cd $projectName/$sample
-	for i in $(find ../../$path|grep fastq)
+	for i in $(find ../../$path/$sample|grep fastq)
 	do
 		ln -s $i
 	done
