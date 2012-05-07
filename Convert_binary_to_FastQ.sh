eb801e84c72ec41795f9441dab493ad09649c13d
Sébastien Boisvert <seb@boisvert.info>
2012-05-07 16:42:28 -0400

    Relative paths are better.

--- Convert_binary_to_FastQ.sh
+++ Convert_binary_to_FastQ.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-#$ -N convert-120321_M00212_0002_AMS2002881-00300
+#$ -N convert-MiSeq
 #$ -P nne-790-ab
 #$ -l h_rt=48:00:00
 #$ -pe default 8
 #$ -cwd
 
-sequenceWorld=/rap/nne-790-ab/Instruments/Illumina_MiSeq_Tombstone/
-run=120321_M00212_0002_AMS2002881-00300
 NSLOTS=8
 
-outputDirectory=$sequenceWorld/$run/Convert_binary_to_FastQ
+outputDirectory=Convert_binary_to_FastQ
 
 source /rap/nne-790-ab/software/CASAVA_v1.8.2/module-load.sh
 
 configureBclToFastq.pl \
---input-dir $sequenceWorld/$run/Data/Intensities/BaseCalls \
+--input-dir Data/Intensities/BaseCalls \
 --output-dir  $outputDirectory \
 --use-bases-mask Y*,Y*,Y*,Y*
 
-mkdir -p $outputDirectory
 cd $outputDirectory
 
 make -j $NSLOTS
